7f648e54dc8b6ed9ed79aaa5f99fa18cf591a093
OpusNocturn <gabriel.pimentel.sotc@gmail.com>
2018-08-16 22:19:51 -0300

    novosprojetosadicionados

---
+++ javascript/myScript.js
@@ -0,0 +1,7 @@
+function myFunction(){
+	console.log("function myFunction(){... rodou");
+	document.getElementById('demo').innerHTML = "javascript rodou";
+	}
+	function outraFuncao(){
+	console.log("vc so vai ver isso no console se chamar a funcao");
+}
