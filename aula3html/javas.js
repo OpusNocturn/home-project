c369f70e1398aa2e12aaea5b4bac5257b5fd1ce9
OpusNocturn <42254419+OpusNocturn@users.noreply.github.com>
2018-08-23 22:37:03 -0300

    Add files via upload

---
+++ aula3html/javas.js
@@ -0,0 +1,42 @@
+/*function SomaDoisNumeros() {
+	var a = 1;
+	var b = 2;
+	var soma = a + b;
+	
+	alert(soma);
+}
+SomaDoisNumeros();
+
+
+function Multiplicacao() {
+	var c = 5;
+	var d = 5;
+	var multiplicacao= c * d;
+	
+	alert(multiplicacao);
+}
+Multiplicacao();
+
+///////////////// a função abaixo funciona
+
+function divi() {
+	var c = 53;
+	var d = 5;
+	var sao= c * d;
+	
+	alert(sao);
+}
+divi();
+
+alert(divi);
+
+*/
+
+function SomaDoisNumeros() {
+	var a = "ontem";
+	var b = 2;
+	var soma = a * b;
+	
+	alert(soma);
+}
+SomaDoisNumeros();
